60afa99d9f39e0dae529c7ae3653a9953fcfec1b
Josipa Mrša <jmrsa@pmfst.hr>
2021-07-24 11:42:18 +0200

    procfile

--- utils/config.js
+++ utils/config.js
@@ -7,6 +7,6 @@ const DBNAME = process.env.NODE_ENV === 'test' ?
     'rn-chat-test-api' :
     'rn-chat-api';
 
-const DB_URI = "mongodb+srv://pmfst-rn:" + PASS + "@chatcluster.pla1j.mongodb.net/" + DBNAME + "?retryWrites=true&w=majority";
+const DB_URI = `mongodb+srv://pmfst-rn:${PASS}@chatcluster.pla1j.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
 
-module.exports = { PORT, DB_URI };
+module.exports = {PORT, DB_URI};
