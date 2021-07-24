ab98cb83c92f48df7123e08f32c04c78a96c861e
Josipa Mrša <jmrsa@pmfst.hr>
2021-07-24 12:04:54 +0200

    procfile

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const io = require('socket.io')(server, {
     }
 });
 
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
+
 //----SOCKET.IO DOGAĐAJI----//
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 const USER_VERIFIED = "userVerified";
